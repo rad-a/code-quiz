0f4591648d9dcf3e627fdd39b3c6d71ab11a44b8
rad-a <11688089+rad-a@users.noreply.github.com>
2020-05-13 07:17:42 -0500

    Updating JS pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+TODO
+//1. Create array of 10 questions made up of objects(multiple choices)
+//2. When user clicks Start Test button, timer starts & first question is displayed
+    //1a. Timer runs down
+//3. When user selects an answer, 
+    //3a. If answer is correct, next question is displayed 
+    //3b. If answer is incorrect, 15 seconds is subtracted from timer
+//4. Repeat step 2 until last question of array
+//5. When timer reaches 0 or user runs out of questions:
+    //5a. append a "Time's up" image to #game-over-screen
+    //5b. Display #save-initials
+    //5c. Save timer, score, and initials to local storage
